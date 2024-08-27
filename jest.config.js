e6c88e1c8a26f9d82c33ddc69769ff3659e7da1d
Ryma <mr_felkir@esi.dz>
2024-08-27 13:55:10 +0100

    test chatbot passed

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    moduleNameMapper: {
-      '^@assets/(.*)$': '<rootDir>/src/assets/$1',
-      '^views/(.*)$': '<rootDir>/src/views/$1',
-      '^components/(.*)$': '<rootDir>/src/components/$1', // Add this line
-    },
-  };
-  
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  moduleNameMapper: {
+    "^@assets/(.*)$": "<rootDir>/src/assets/$1",
+    "^views/(.*)$": "<rootDir>/src/views/$1",
+    "^components/(.*)$": "<rootDir>/src/components/$1",
+    "\\.(css|less|sass|scss)$": "<rootDir>/styleMock.js",
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"], // Enable this line
+};
